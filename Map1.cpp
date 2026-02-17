aba8ca537296f78eac706bae4c0fa282776787dc
Boriphat1581 <boriphat_palee@cmu.ac.th>
2026-02-17 15:08:37 +0700

    Map1

--- Map1.cpp
+++ Map1.cpp
@@ -1,6 +1,64 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
 #include <iostream>
-using namespace std;
 
-int main() {
-cout << "For Map1" << endl;
+std::vector<std::vector<int>> mapLayout = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,1,0,0,1,1,1,0,0,1,0,1},
+    {1,0,1,0,0,0,0,0,0,1,0,0,1,0,1},
+    {1,0,1,1,1,0,0,1,1,1,0,0,1,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+int main()
+{
+   sf::RenderWindow window(
+    sf::VideoMode({800u, 600u}),
+    "SFML Map Technique",
+    sf::Style::Default
+);
+
+    float cellSize = 16.f;
+    sf::RectangleShape wall({cellSize, cellSize});
+    wall.setFillColor(sf::Color::Red);
+
+    std::vector<sf::RectangleShape> walls;
+
+    for (int y = 0; y < mapLayout.size(); y++) {
+        for (int x = 0; x < mapLayout[y].size(); x++) {
+            if (mapLayout[y][x] == 1) {
+               wall.setPosition({ x * cellSize, y * cellSize });
+
+                walls.push_back(wall);
+            }
+        }
+    }
+
+    sf::Texture mapTexture;
+    if (!mapTexture.loadFromFile("map_design.png")) {
+        std::cout << "Could not load map image\n";
+    }
+
+    sf::Sprite mapSprite(mapTexture);
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(mapSprite);
+
+        for (auto& w : walls)
+            window.draw(w);
+
+        window.display();
+    }
+
+    return 0;
 }
